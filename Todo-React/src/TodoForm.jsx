58de478377c62c736325a4a1535e0c8275a18079
ayanchavand <ayan.chavand15@gmail.com>
2024-08-14 12:03:00 +0530

    Todo Components

---
+++ Todo-React/src/TodoForm.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+
+  
+export default function TodoForm(addTodo) {
+    const [input, setInput] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(input)
+        setInput('')
+    }
+
+    return (
+      <Box component = "form" 
+        sx={{
+          width: 500,
+          maxWidth: '100%',
+        }}
+        onSubmit={handleSubmit}
+      >
+        <TextField fullWidth label="Da faq you wanna DO?" id="fullWidth" value={input} onChange={(e) => {setInput(e.target.value)}}/>
+        <Button variant="contained">Submit</Button>
+      </Box>
+    );
+}
---
+++ Todo-React/src/TodoList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+ export default function TodoList(){
+    return (
+      <>
+      <ul>
+        <li>hii</li>
+      </ul>
+      </>
+    )
+}
